61b540fa6ab2865e8323951dcb0bd2b26f4866eb
Emilian Gradinarov <hoff_essex@abv.bg>
2016-04-19 23:25:15 +0300

    removed update method from gameInfo

--- TicTacToe/src/game/databases/GameInfo.java
+++ TicTacToe/src/game/databases/GameInfo.java
@@ -9,10 +9,6 @@ import java.util.List;
 public class GameInfo implements Serializable {
     private List<Player> players;
 
-    public void update(Player player) {
-        player.updatePoints();
-    }
-
     public String getInfo() {
         String info = String.format("%s : %s",
                 players.get(0).toString(), players.get(1).toString());
