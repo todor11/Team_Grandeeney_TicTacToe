21103c879a9aecc5c40cf063bc26a77586d1b2f2
Emilian Gradinarov <hoff_essex@abv.bg>
2016-04-19 22:07:15 +0300

    updated - gameInfo

--- TicTacToe/src/game/databases/GameInfo.java
+++ TicTacToe/src/game/databases/GameInfo.java
@@ -4,22 +4,19 @@ import java.io.Serializable;
 import java.util.List;
 
 public class GameInfo implements Serializable {
-    private List<String> playersNames;
-    private List<Integer> temporaryScore;
-    private int moveCounter;
+    private List<Player> players;
 
     public void update(Player player) {
         player.updatePoints();
     }
 
     public String getInfo() {
-        String info = String.Format("%s - $d ; %s -  %d",
-                playersNames.get(0), temporaryScore.get(0), playersNames.get(1), temporaryScore.get(1));
+        String info = String.Format("%s : %s",
+                players.get(0).toString(), players.get(1).toString());
         return info;
     }
 
-    public void addPlayer(String name) {
-        playersNames.add(name);
+    public void addPlayer(Player player) {
+        players.add(player);
     }
-
 }
